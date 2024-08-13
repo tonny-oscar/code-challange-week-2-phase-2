600598608fbc263186f8b7ba474d7e11d4df972b
Tonny Bett <betttonny26@gmail.com>
2024-08-13 11:33:46 +0300

    inserted the bot collection and bots

--- Bot Battlr/src/App.jsx
+++ Bot Battlr/src/App.jsx
@@ -1,16 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import BotCollection from "./components/BotCollection";
+import BotArmy from "./components/BotArmy";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const [myBots, setBots] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/bots")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  //delete request
+  const dischargeBot = (id) => {
+    releaseBot(id);
+    console.log("remove from db");
+  };
+
+  // enlist bot
+  const enlistBot = (bot) => {
+    const findBot = myBots.find((b) => b.id === bot.id);
+    if (findBot === undefined) {
+      setBots([...myBots, bot]);
+    }
+  };
+
+  const releaseBot = (id) => {
+    // filter
+    console.log("release from army");
+  };
 
   return (
-    <div>
-      
-    </div>
-  )
+    <main>
+      <h1>My bot army</h1>
+      <BotArmy myBots={myBots} releaseBot={releaseBot} />
+      <h1>Bot collection</h1>
+      <BotCollection
+        data={data}
+        enlistBot={enlistBot}
+        dischargeBot={dischargeBot}
+      />
+    </main>
+  );
 }
 
-export default App
+export default App.jsx;
